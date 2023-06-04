3f1899067573c42dc9c6d614b0a49fe0d7e7c2eb
ddfilipov <denisfilipov12+3@gmail.com>
2023-06-04 18:31:06 +0200

    added stuff in backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,11 +3,17 @@ import * as bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
+const port = 5000;
+// const port = process.env.PORT || 3000;
+
+// enable json in the request body
+app.use(bodyParser.json());
+
+// enable cross origin resourse sharing
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hi!");
 });
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App listening to PORT: ${port}`));

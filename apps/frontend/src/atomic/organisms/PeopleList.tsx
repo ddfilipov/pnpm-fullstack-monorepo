bd09ce17add249a3e4b5411b8aeb26d05a02feb3
ddfilipov <denisfilipov12+3@gmail.com>
2023-06-11 15:52:37 +0200

    list is just a list now

--- apps/frontend/src/atomic/organisms/PeopleList.tsx
+++ apps/frontend/src/atomic/organisms/PeopleList.tsx
@@ -1,18 +1,13 @@
 import { FC } from "react";
 import { IPersonData } from "@pnpm-fullstack-monorepo/validation";
 import { styled } from "styled-components";
+import PersonCard from "../molecules/PersonCard";
 
 const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     gap: 10px;
-`;
-
-const StyledBox = styled.div`
-    padding: 10px;
-    width: 300px;
-    border: 1px solid black;
 `;
 
 interface PeopleListProps {
@@ -23,16 +18,7 @@ const PeopleList: FC<PeopleListProps> = ({ people }) => {
     return (
         <Wrapper>
             {people?.map((person) => {
-                return (
-                    <StyledBox key={person.id}>
-                        <p>{person.name}</p>
-                        <ul>
-                            <li>{person.money}</li>
-                            <li>{person.birthDate.toString()}</li>
-                            {person.vip ? <li>VIP</li> : null}
-                        </ul>
-                    </StyledBox>
-                );
+                return <PersonCard person={person} />;
             })}
         </Wrapper>
     );

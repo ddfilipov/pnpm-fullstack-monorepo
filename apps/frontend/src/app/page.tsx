a9570d9d79dcbee1ff6a1adf7e0bcb652270ed9c
ddfilipov <denisfilipov12+3@gmail.com>
2023-06-11 14:15:49 +0200

    added some text

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -5,6 +5,8 @@ import Temporal from "../atomic/organisms/temporary";
 export default function Home() {
     return (
         <>
+            <h1>People</h1>
+            <p>You can add, delete and edit people.</p>
             <Temporal />
         </>
     );

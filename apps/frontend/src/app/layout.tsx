98f87262602319ed59b96d148b96ea9482631c96
ddfilipov <denisfilipov12+3@gmail.com>
2023-06-13 19:26:57 +0200

    trynna center the app

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body>
                 <div className="container">{children}</div>
             </body>
         </html>

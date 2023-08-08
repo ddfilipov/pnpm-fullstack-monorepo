4a5bce8e51356f9c24b0d3672253dfd9d7399e8a
ddfilipov <denisfilipov12+3@gmail.com>
2023-08-08 18:27:28 +0200

    small explanation on what I did on the code

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -2,7 +2,7 @@ export interface IPersonDataResponse {
     people: IPersonData[];
 }
 export interface IPersonData {
-    personId: number; // TODO: should change this to personId or something so react-hook-form's id doesn't complain
+    personId: number; // changed from id to personId react-hook-form's useFieldArray id doesn't complain
     name: string;
     dateOfBirth: Date;
     money: number;
